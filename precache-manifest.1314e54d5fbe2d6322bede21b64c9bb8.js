08cc828fb8a8c5dbd92750399a914bb86c0441e6
unknown <villegasoshin25@gmail.com>
2019-01-07 09:46:21 -0500

    Updates

---
+++ precache-manifest.1314e54d5fbe2d6322bede21b64c9bb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "143d0e7a8cc00c3a5a27",
+    "url": "/static/js/main.143d0e7a.chunk.js"
+  },
+  {
+    "revision": "25aad3231a6012c14f1c",
+    "url": "/static/js/1.25aad323.chunk.js"
+  },
+  {
+    "revision": "143d0e7a8cc00c3a5a27",
+    "url": "/static/css/main.68c500cd.chunk.css"
+  },
+  {
+    "revision": "4292e143f3241beb11957e1a181072ff",
+    "url": "/index.html"
+  }
+];
